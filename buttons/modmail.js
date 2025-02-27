811baa652496ba6424648e1f15e4e36967c0d491
Music Maker <joshuanejdl@gmail.com>
2025-02-27 14:57:37 -0600

    fix: Modmail transcripts will exclude background staff chatter

--- buttons/modmail.js
+++ buttons/modmail.js
@@ -5,7 +5,8 @@ const  {
 	ActionRowBuilder
 } = require('discord.js');
 
-const { createTranscript } = require('discord-html-transcripts');
+const { generateFromMessages } = require('discord-html-transcripts');
+const config = require('../modmail-services/config');
 
 const staffRoles = [
 	'970775928701603841', // Moderator
@@ -90,7 +91,26 @@ module.exports = {
 				embeds: [embed]
 			});
 
-			const transcript = await createTranscript(channel, {
+			const channelMessages = await FetchBulkMessages(channel, 1000);
+
+			let cleanMessages = [];
+			for (let i = 0; i < channelMessages.length; i++) {
+				const message = channelMessages[i];
+
+				if (message.content && message.content[0] !== config.guild.sendPrefix) {
+					continue;
+				}
+
+				if (message.content) message.content = message.content.replace(new RegExp(`^\\${config.guild.sendPrefix}\s*`), '');
+
+				message.createdAt = new Date(message.createdTimestamp);
+
+				cleanMessages[i] = message;
+			}
+
+			cleanMessages = cleanMessages.filter(Boolean).sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+
+			const transcript = await generateFromMessages(cleanMessages, channel, {
 				limit: -1,
 				returnType: 'attachment',
 				filename: `transcript-${user.globalName || user.username}.html`,
@@ -119,3 +139,29 @@ module.exports = {
 		}
 	}
 };
+
+
+async function FetchBulkMessages(channel, messageCount) {
+	const messages = [];
+
+	let lastMessageID = null;
+
+	while (messages.length < messageCount) {
+		const options = {
+			limit: Math.min(messageCount - messages.length, 100)
+		};
+
+		if (lastMessageID) {
+			options.before = lastMessageID;
+		}
+
+		const fetchedMessages = await channel.messages.fetch(options);
+		if (fetchedMessages.size === 0) break;
+
+		messages.push(...fetchedMessages.values());
+		lastMessageID = fetchedMessages.last().id;
+	}
+
+	return messages;
+
+}
